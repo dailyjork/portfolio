a6572d63aa5a42cb13b6fb8b0432177e87821d24
DESKTOP-GINN164\Thijs <251917@student.scalda.nl>
2026-03-02 11:34:50 +0100

    add main files/frame

---
+++ script/main.js
@@ -0,0 +1,43 @@
+const expierence = document.getElementById('expierence');
+const projects = document.getElementById('projects');
+const about = document.getElementById('aboutMe');
+const skills = document.getElementById('skills');
+const contact = document.getElementById('contact');
+
+function goDiv(id) {
+    window.scroll(0, findPos(id));
+}
+
+function findPos(div) {
+    let currenttop = 0;
+    if (div.offsetParent) {
+        do {
+            currenttop += div.offsetTop;
+        } while ((div = div.offsetParent));
+        return [currenttop];
+    }
+}
+
+
+const theme = document.getElementById('cMode')
+
+
+theme.addEventListener ("click", function() { 
+    switchMode();
+});
+
+const body = document.body;
+const gitLogo = document.getElementById('git');
+
+function switchMode() {
+    body.classList.toggle('light-mode');
+    if (body.classList.contains('light-mode')) {
+        gitLogo.innerHTML =  '<a href="https://github.com/dailyjork" target="_blank"><img src="../images/githubLight.png"></img></a>'
+        theme.innerText = ("Light mode");
+        console.log("Modus: licht");
+    } else {
+        gitLogo.innerHTML = '<a href="https://github.com/dailyjork" target="_blank"><img src="../images/github.png"></img></a>'
+        theme.innerText = ("Dark mode");
+        console.log("Modus: donker");
+    }
+}
