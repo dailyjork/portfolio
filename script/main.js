ec3275868c71b4a923a79b77043765c76d066c05
DESKTOP-GINN164\Thijs <251917@student.scalda.nl>
2026-03-02 23:36:45 +0100

    Added information to About me and socials also added a button to go to cat pictures

--- script/main.js
+++ script/main.js
@@ -24,21 +24,24 @@ function findPos(div) {
 const theme = document.getElementById('cMode')
 
 
-theme.addEventListener ("click", function() { 
+theme.addEventListener("click", function () {
     switchMode();
 });
 
 const body = document.body;
 const gitLogo = document.getElementById('git');
+const gitContact = document.getElementById('gitC');
 
 function switchMode() {
     body.classList.toggle('light-mode');
     if (body.classList.contains('light-mode')) {
-        gitLogo.innerHTML =  '<a href="https://github.com/dailyjork" target="_blank"><img src="images/githubLight.png"></img></a>'
+        gitLogo.innerHTML = '<a href="https://github.com/dailyjork" target="_blank"><img src="images/githubLight.png"></a>'
+        gitContact.innerHTML = '<img src="images/githubLight.png">'
         theme.innerText = ("Light mode");
         console.log("Modus: licht");
     } else {
-        gitLogo.innerHTML = '<a href="https://github.com/dailyjork" target="_blank"><img src="images/github.png"></img></a>'
+        gitLogo.innerHTML = '<a href="https://github.com/dailyjork" target="_blank"><img src="images/github.png"></a>'
+        gitContact.innerHTML = '<img src="images/github.png">'
         theme.innerText = ("Dark mode");
         console.log("Modus: donker");
     }
